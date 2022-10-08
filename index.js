96e426bb241eca8f5f3418d274fb50acd2019193
JohnnyBoySou <joaodesousa101@gmail.com>
2022-10-08 10:02:37 -0300

    add cors

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import { connect } from 'mongoose';
 import routes from "./routes.js";
 import bodyParser from 'body-parser'
+import cors from 'cors'
+
 
 const app = express()
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors)
 
 try{
     connect(`mongodb+srv://joaodesousa101:223761dejoao@cluster0.ucfm4zm.mongodb.net/?retryWrites=true&w=majority`, {
